4abf63360e0b531f07acc2b2045fddef9b5f2889
jarod <j.sengkeo@student.isartdigital.com>
2021-03-14 02:24:06 +0100

    Platformer2D bumper

---
+++ Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    public float liftSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO : meme systeme que EnemyMove
+        transform.position += Vector3.up * Mathf.Cos(Time.time) * 4 * liftSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int health = 5;
+    private int maxHealth;
+
+    private float dmgTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage()
+    {
+        if (Time.time - dmgTime < 1f)
+            return;
+
+        dmgTime = Time.time;
+
+        if (--health <= 0)
+        {
+            SceneManager.LoadScene("Platformer2D");
+        }
+    }
+
+    public bool heal()
+    {
+        if (health >= maxHealth)
+            return false;
+
+        HealthDisplay hd = GameObject.Find("Health Panel").GetComponent<HealthDisplay>();
+        hd.instantiateHeal(++health);
+
+        return true;
+    }
+}
